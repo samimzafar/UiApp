c0112d306b7b37d71bb6b7c1b96cc0f72bdc9dc3
sameem.z@dplit.com-at-401940162791 <sameem.z@dplit.com>
2024-03-24 02:39:11 +0500

    changes

--- index.js
+++ index.js
@@ -5,11 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {QueryClient, QueryClientProvider} from 'react-query';
-const queryClient = new QueryClient();
-const RootComponent = () => (
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-);
+const RootComponent = () => <App />;
 AppRegistry.registerComponent(appName, () => RootComponent);
