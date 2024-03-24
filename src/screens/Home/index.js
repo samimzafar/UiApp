e6271671909ac4082fd4b26c8c95b2c70d22baa9
sameem.z@dplit.com-at-401940162791 <sameem.z@dplit.com>
2024-03-24 17:10:28 +0500

    changes

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {styles} from './styles';
+import Icon from 'react-native-vector-icons/AntDesign';
 export const Home = ({navigation}) => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Icon name="user" size={30} color="white" />
+    </View>
+  );
 };
